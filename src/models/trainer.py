3fae9e7bfa2e038e8e45eb805c9857caf59240ea
Seonghyeon Ye <vano1205@naver.com>
2020-11-23 15:41:59 +0900

    final version (revised loss and ekman)

--- src/models/trainer.py
+++ src/models/trainer.py
@@ -15,6 +15,7 @@ from transformers import AdamW, get_linear_schedule_with_warmup
 from pytorch_pretrained_bert import BertAdam
 
 from data import EmotionDataset
+import pandas as pd
 
 
 
@@ -58,7 +59,12 @@ class EMDLoss(torch.nn.Module):
         self.a_sorted_idxs = torch.tensor(np.argsort(a_scores).tolist()).to(self.args['device'])
         d_scores = [self.category_label_vads[key][2] for key in self.category_label_names]
         self.d_sorted_idxs = torch.tensor(np.argsort(d_scores).tolist()).to(self.args['device'])
-
+        self.v_sorted_values = torch.tensor(np.sort(v_scores).tolist()).to(self.args['device'])
+        self.a_sorted_values = torch.tensor(np.sort(a_scores).tolist()).to(self.args['device'])
+        self.d_sorted_values = torch.tensor(np.sort(d_scores).tolist()).to(self.args['device'])
+        # print("sorted value v", self.v_sorted_values)
+        # print("sorted value a", self.a_sorted_values)
+        # print("sorted value d", self.d_sorted_values)
 
     def _sort_labels_by_vad_coordinates(self, labels):
         v_labels = torch.index_select(labels, 1, self.v_sorted_idxs)
@@ -66,20 +72,41 @@ class EMDLoss(torch.nn.Module):
         d_labels = torch.index_select(labels, 1, self.d_sorted_idxs)
         return v_labels, a_labels, d_labels
 
+    def _set_vad_distance_matrix(self):
+        v_distance_vector = torch.roll(self.v_sorted_values, -1, 0) - self.v_sorted_values 
+        for idx, v_distance_element in enumerate(v_distance_vector):
+            if v_distance_element == 0:
+                assert idx != len(v_distance_vector)-1
+                v_distance_vector[idx] = v_distance_vector[idx+1]     
+        v_distance_vector[-1]=0
+        a_distance_vector = torch.roll(self.a_sorted_values, -1, 0) - self.a_sorted_values
+        for idx, a_distance_element in enumerate(a_distance_vector):
+            if a_distance_element == 0:
+                assert idx != len(a_distance_vector)-1
+                a_distance_vector[idx] = a_distance_vector[idx+1] 
+        a_distance_vector[-1]=0
+        d_distance_vector = torch.roll(self.d_sorted_values, -1, 0) - self.d_sorted_values
+        for idx, d_distance_element in enumerate(d_distance_vector):
+            if d_distance_element == 0:
+                assert idx != len(d_distance_vector)-1
+                d_distance_vector[idx] = d_distance_vector[idx+1] 
+        d_distance_vector[-1]=0
+        return v_distance_vector, a_distance_vector, d_distance_vector
 
     def _intra_EMD_loss(self, input_probs, label_probs):
-        intra_emd_loss = torch.sum(
-            torch.square(input_probs - label_probs), dim=1)
-        return intra_emd_loss
+        intra_emd_loss = torch.div( torch.sum(
+            torch.square(input_probs - label_probs), dim=1), len(self.category_label_names))
+        return intra_emd_loss 
 
 
-    def _inter_EMD_loss(self, input_probs, label_probs):
+    def _inter_EMD_loss(self, input_probs, label_probs, distance):
         normalized_input_probs = input_probs / (torch.sum(input_probs, keepdim=True, dim=1) + self.eps)
-        normalized_label_probs = label_probs / (torch.sum(label_probs, keepdim=True, dim=1) + self.eps)   
-        inter_emd_loss = torch.sum(
-            torch.square(
+        normalized_label_probs = label_probs / (torch.sum(label_probs, keepdim=True, dim=1) + self.eps)  
+
+        # multiply vad distance weight to subtraction of cumsum
+        inter_emd_loss = torch.matmul(distance, torch.transpose(torch.square(
                 torch.cumsum(normalized_input_probs, dim=1) - torch.cumsum(normalized_label_probs, dim=1),
-            ), dim=1)
+            ), 0, 1))
         return inter_emd_loss
 
 
@@ -95,12 +122,13 @@ class EMDLoss(torch.nn.Module):
 
         split_logits = torch.split(logits, len(self.category_label_names), dim=1) # logits for sorted (v, a, d)
         sorted_labels = self._sort_labels_by_vad_coordinates(labels)              # labels for sorted (v, a, d)
+        distance_labels = self._set_vad_distance_matrix()
 
         if self.args['use_emd']:
             losses = []
-            for logit, sorted_label in zip(split_logits, sorted_labels):
+            for logit, sorted_label, distance_label in zip(split_logits, sorted_labels, distance_labels):
                 input_probs = self.activation(logit)
-                inter_emd_loss = self._inter_EMD_loss(input_probs, sorted_label)
+                inter_emd_loss = self._inter_EMD_loss(input_probs, sorted_label, distance_label)
                 intra_emd_loss = self._intra_EMD_loss(input_probs, sorted_label)
                 emd_loss = inter_emd_loss + intra_emd_loss
                 losses.append(emd_loss)
@@ -231,7 +259,7 @@ class Trainer():
 
     def _set_loss(self):
         if self.args['task'] == 'category-classification':
-            assert self.args['dataset'] in ['semeval', 'ssec', 'isear', 'goemotions']
+            assert self.args['dataset'] in ['semeval', 'ssec', 'isear', 'goemotions', 'ekman']
             if self.args['dataset'] == 'semeval': # multi-labeled
                 self.loss = torch.nn.BCEWithLogitsLoss()
             elif self.args['dataset'] == 'ssec': # multi-labeled
@@ -240,6 +268,8 @@ class Trainer():
                 self.loss = torch.nn.CrossEntropyLoss()
             elif self.args['dataset'] == 'goemotions': # multi-labeled
                 self.loss = torch.nn.BCEWithLogitsLoss()
+            elif self.args['dataset'] == 'ekman': # multi-labeled
+                self.loss = torch.nn.BCEWithLogitsLoss()
             
 
         elif self.args['task'] == 'vad-regression':
@@ -247,7 +277,7 @@ class Trainer():
             self.loss = nn.MSELoss()
 
         elif self.args['task'] == 'vad-from-categories':
-            assert self.args['dataset'] in ['semeval', 'ssec', 'isear', 'goemotions']
+            assert self.args['dataset'] in ['semeval', 'ssec', 'isear', 'goemotions', 'ekman']
             if self.args['dataset'] == 'semeval': # multi-labeled
                 self.loss = EMDLoss(self.args, label_type='multi')
             elif self.args['dataset'] == 'ssec': # multi-labeled
@@ -256,10 +286,12 @@ class Trainer():
                 self.loss = EMDLoss(self.args, label_type='single')
             elif self.args['dataset'] == 'goemotions': # multi-labeled
                 self.loss = EMDLoss(self.args, label_type='multi')
+            elif self.args['dataset'] == 'ekman': # multi-labeled
+                self.loss = EMDLoss(self.args, label_type='multi')  
 
 
     def _set_prediction(self):
-        assert self.args['dataset'] in ['semeval', 'ssec', 'isear', 'goemotions']
+        assert self.args['dataset'] in ['semeval', 'ssec', 'isear', 'goemotions', 'ekman']
         if self.args['dataset'] == 'semeval': # multi-labeled
             self.convert_logits_to_predictions = PredcitVADandClassfromLogit(self.args, label_type='multi')
         elif self.args['dataset'] == 'ssec': # multi-labeled
@@ -268,20 +300,51 @@ class Trainer():
             self.convert_logits_to_predictions = PredcitVADandClassfromLogit(self.args, label_type='single')
         elif self.args['dataset'] == 'goemotions': # multi-labeled
             self.convert_logits_to_predictions = PredcitVADandClassfromLogit(self.args, label_type='multi')
+        elif self.args['dataset'] == 'ekman': # multi-labeled
+            self.convert_logits_to_predictions = PredcitVADandClassfromLogit(self.args, label_type='multi')
         
 
     def _set_eval_layers(self):
         self.softmax = nn.Softmax(dim=1)
         self.sigmoid = nn.Sigmoid()
 
+    def _anneal(self, param):
+        x = np.linspace(param[0], param[1], num=15)
+        return np.exp(x).tolist()
 
-    def compute_loss(self, logits, labels):
+    def _roll_seq(self, x, dim=1, shift=1):
+        length = x.size(dim) - shift
+
+        seq = torch.cat([x.narrow(dim, shift, length),
+                         torch.zeros_like(x[:, :1])], dim)
+
+        return seq
+
+
+    def compute_loss(self, inputs, lm_logits, logits, labels):
         if self.args['task'] == 'vad-regression':
             logits = F.relu(logits)
+            overall_loss = self.loss(logits, labels)
         if self.args['task'] == 'category-classification':
-            if self.args['dataset'] == 'semeval' or self.args['dataset'] == 'ssec' or self.args['dataset'] == 'goemotions' : # multi-labeled
+            if self.args['dataset'] == 'semeval' or self.args['dataset'] == 'ssec' or self.args['dataset'] == 'goemotions' or self.args['dataset'] == 'ekman' : # multi-labeled
                 labels = labels.type_as(logits)
-        return self.loss(logits, labels)
+                overall_loss = self.loss(logits, labels)
+            else:
+                overall_loss = self.loss(logits, labels)
+        if self.args['task'] == 'vad-from-categories':
+            emd_loss = self.loss(logits, labels)
+            exp_decay=[-3.9, -4.6]
+            self.lm_coef = exp_decay
+            self.lm_coef = self._anneal(self.lm_coef)
+            try:
+                lm_coef = self.lm_coef[self.coef_step]
+            except:
+                lm_coef = self.lm_coef[-1]
+            
+            lm_criterion = nn.CrossEntropyLoss(ignore_index=0)
+            lm_loss = lm_criterion(lm_logits.contiguous().view(-1, lm_logits.size(-1)),self._roll_seq(inputs).contiguous().view(-1))
+            overall_loss = lm_loss * lm_coef + emd_loss
+        return overall_loss
 
 
     # https://huggingface.co/transformers/migration.html?highlight=forsequenceclassification
@@ -342,7 +405,7 @@ class Trainer():
             labels, 
             predictions, 
             digits=4,
-            zero_division='warn')
+            zero_division='warn', output_dict=True)
         metrics['classification'] = report
         if add_jaccard_score:
             metrics['jaccard_score'] = {}
@@ -368,7 +431,7 @@ class Trainer():
             if eval_type == 'vad':
                 metrics = self._compute_vad_eval_metrics(metrics, predictions, labels)
             else: # eval_type == 'cat':
-                if self.args['dataset'] in ['semeval', 'ssec', 'goemotions']: # multi-labeled
+                if self.args['dataset'] in ['semeval', 'ssec', 'goemotions', 'ekman']: # multi-labeled
                     add_jaccard_score = True
                 elif self.args['dataset'] == 'isear': # single-labeled
                     add_jaccard_score = False
@@ -380,7 +443,7 @@ class Trainer():
         
         # 3. category-classification ------------------------------------------------
         elif self.args['task'] == 'category-classification':
-            if self.args['dataset'] in ['semeval', 'ssec', 'goemotions']: # multi-labeled
+            if self.args['dataset'] in ['semeval', 'ssec', 'goemotions', 'ekman']: # multi-labeled
                 add_jaccard_score = True
             elif self.args['dataset'] == 'isear': # single-labeled
                 add_jaccard_score = False
@@ -410,38 +473,41 @@ class Trainer():
                 labels = batch[2].to(torch.device(self.args['device']))
                 total_labels.append(labels)
 
-                logits = model(
+                lm_logits, cls_logits = model(
                     input_ids,
                     attention_mask=attention_masks)
 
                 # 2. compute loss (objective)
                 if compute_loss:
-                    eval_loss = self.compute_loss(logits, labels)
+                    eval_loss = self.compute_loss(input_ids, lm_logits, cls_logits, labels)
                     total_losses += torch.sum(eval_loss)
                 
                 # 3. model predictions
                 if self.args['task'] == 'vad-regression':
-                    predictions = F.relu(logits) # vads
+                    predictions = F.relu(cls_logits) # vads
 
                 elif self.args['task'] == 'vad-from-categories':
                     if prediction_type == 'vad':
-                        predictions = self.convert_logits_to_predictions(logits, predict='vad')         # vad 
+                        predictions = self.convert_logits_to_predictions(cls_logits, predict='vad')         # vad 
                     else: # prediction_type == 'cat':
-                        predictions = self.convert_logits_to_predictions(logits, predict='cat')         # vad 
+                        predictions = self.convert_logits_to_predictions(cls_logits, predict='cat')         # vad 
 
                 elif self.args['task'] == 'category-classification':
-                    assert self.args['dataset'] in ['semeval', 'ssec', 'isear', 'goemotions']
+                    assert self.args['dataset'] in ['semeval', 'ssec', 'isear', 'goemotions', 'ekman']
                     if self.args['dataset'] == 'semeval': # multi-labeled
-                        predictions = self.sigmoid(logits) >= 0.5
+                        predictions = self.sigmoid(cls_logits) >= 0.5
                         predictions = torch.squeeze(predictions.float()) 
                     elif self.args['dataset'] == 'ssec': # multi-labeled
-                        predictions = self.sigmoid(logits) >= 0.5
+                        predictions = self.sigmoid(cls_logits) >= 0.5
                         predictions = torch.squeeze(predictions.float()) 
                     elif self.args['dataset'] == 'isear': # single-labeled
-                        predictions = self.softmax(logits)
+                        predictions = self.softmax(cls_logits)
                         predictions = torch.max(predictions, 1)[1] # argmax along dim=1
                     elif self.args['dataset'] == 'goemotions': # multi-labeled
-                        predictions = self.sigmoid(logits) >= 0.5
+                        predictions = self.sigmoid(cls_logits) >= 0.5
+                        predictions = torch.squeeze(predictions.float())
+                    elif self.args['dataset'] == 'ekman': # multi-labeled
+                        predictions = self.sigmoid(cls_logits) >= 0.5
                         predictions = torch.squeeze(predictions.float())
 
                 total_predictions.append(predictions)
